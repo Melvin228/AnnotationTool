081ac9573c2469028a6e06278a479ce426c4cd5c
Melvin Gan <melvingan2811@gmail.com>
2022-07-20 21:21:35 +0800

    Added dist files

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
 const app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
+app.use("/", serveStatic(path.join(__dirname, "dist")));
 app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "/dist/index.html"));
 });
